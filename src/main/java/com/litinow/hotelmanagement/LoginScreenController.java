44bfbe596f2236349ab93e6b60d60fdd6d00d215
Mauricio Giacomini Penteado <mauricio.penteado@litinow.com>
2018-11-16 23:29:40 +0000

    Enhance login screen design

--- src/main/java/com/litinow/hotelmanagement/LoginScreenController.java
+++ src/main/java/com/litinow/hotelmanagement/LoginScreenController.java
@@ -5,7 +5,9 @@ package com.litinow.hotelmanagement;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -15,15 +17,24 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 /**
  * FXML Controller class
@@ -36,6 +47,9 @@ public class LoginScreenController implements Initializable {
     private TextField username;
     @FXML
     private TextField password;
+    @FXML
+    private StackPane stackerpane;
+    
 
     /**
      * Initializes the controller class.
@@ -43,82 +57,143 @@ public class LoginScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void loginbutton(MouseEvent event) {
-        
-        boolean isExist = false;
-        String userPass = "";
-        String userType = "";
-        String sql = "select * from users where username = '"+username.getText().trim()+"'";
-        Connection connection = DBConnection.getConnection();
-        try {
-            PreparedStatement ps = (PreparedStatement)connection.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
-                isExist = true;
-                userPass = rs.getString(3);
-                userType = rs.getString(9);
-            }
-            if(isExist){
-                
-                if(password.getText().trim().equals(userPass)){
-                    
-                    if(userType.equals("admin")){
-                        //admin screen
-                        
-                        Stage adminScreen = new Stage();
-                        Parent root = null;
-                        try {
-                            root = FXMLLoader.load(getClass().getResource("/fxml/AdminScreen.fxml"));
-                        } catch (IOException ex) {
-                            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        
-                        Stage current = (Stage)username.getScene().getWindow();
-                        Scene scene = new Scene(root,1366,730);
-                        
-                        adminScreen.setScene(scene);
-                        adminScreen.initStyle(StageStyle.TRANSPARENT);
-                        
-                        current.hide();
-                        
-                        adminScreen.show();
-                        
-                        
-                    }else{
-                        //home screen
-                        
-                         Stage homeScreen = new Stage();
-                        Parent root = null;
-                        try {
-                            root = FXMLLoader.load(getClass().getResource("/fxml/HomeScreen.fxml"));
-                        } catch (IOException ex) {
-                            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        
-                        Stage current = (Stage)username.getScene().getWindow();
-                        Scene scene = new Scene(root,1366,730);
-                        
-                        homeScreen.setScene(scene);
-                        homeScreen.initStyle(StageStyle.TRANSPARENT);
-                        
-                        current.hide();
-                        
-                        homeScreen.show();
-                    }
+
+        if (username.getText().equals("")) {
+            Image image = new Image("img/delete.png");
+            Notifications notification = Notifications.create()
+                    .title("Error")
+                    .text("Username Is Empty")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_LEFT)
+                    .graphic(new ImageView(image));
+            notification.darkStyle();
+            notification.show();
+        } else if (password.getText().equals("")) {
+            Image image = new Image("img/delete.png");
+            Notifications notification = Notifications.create()
+                    .title("Error")
+                    .text("Password Is Empty")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_LEFT)
+                    .graphic(new ImageView(image));
+            notification.darkStyle();
+            notification.show();
+        } else {
+            boolean isExist = false;
+            String userPass = "";
+            String userType = "";
+            String sql = "select * from users where username = '" + username.getText().trim() + "'";
+            Connection connection = DBConnection.getConnection();
+            try {
+                PreparedStatement ps = (PreparedStatement) connection.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    isExist = true;
+                    userPass = rs.getString(3);
+                    userType = rs.getString(9);
                 }
-                
+                if (isExist) {
+
+                    if (password.getText().trim().equals(userPass)) {
+
+                        if (userType.equals("admin")) {
+                            //admin screen
+
+                            Stage adminScreen = new Stage();
+                            Parent root = null;
+                            try {
+                                root = FXMLLoader.load(getClass().getResource("/fxml/AdminScreen.fxml"));
+                            } catch (IOException ex) {
+                                Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+
+                            Stage current = (Stage) username.getScene().getWindow();
+                            Scene scene = new Scene(root, 1366, 730);
+
+                            adminScreen.setScene(scene);
+                            adminScreen.initStyle(StageStyle.TRANSPARENT);
+
+                            current.hide();
+
+                            adminScreen.show();
+
+                        } else {
+                            //home screen
+
+                            Stage homeScreen = new Stage();
+                            Parent root = null;
+                            try {
+                                root = FXMLLoader.load(getClass().getResource("/fxml/HomeScreen.fxml"));
+                            } catch (IOException ex) {
+                                Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+
+                            Stage current = (Stage) username.getScene().getWindow();
+                            Scene scene = new Scene(root, 1366, 730);
+
+                            homeScreen.setScene(scene);
+                            homeScreen.initStyle(StageStyle.TRANSPARENT);
+
+                            current.hide();
+
+                            homeScreen.show();
+                        }
+                    }
+
+                } else {
+                    Image image = new Image("img/delete.png");
+                    Notifications notification = Notifications.create()
+                            .title("Error")
+                            .text("Check Your username and password again")
+                            .hideAfter(Duration.seconds(3))
+                            .position(Pos.BOTTOM_LEFT)
+                            .graphic(new ImageView(image));
+                    notification.darkStyle();
+                    notification.show();
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+
         }
-        
-        
-        
+
     }
     
+     @FXML
+    private void cancelButton(MouseEvent event) {
+        
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        dialogLayout.setHeading(new Text("Close"));
+        dialogLayout.setBody(new Text("Do you want to exit?"));
+        
+        JFXButton ok = new JFXButton("OK");
+        JFXButton cancel = new JFXButton("Cancel");
+        
+        JFXDialog dialog = new JFXDialog(stackerpane, dialogLayout, JFXDialog.DialogTransition.CENTER);
+        
+        ok.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               System.exit(0);
+            }
+        });
+        
+        cancel.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                dialog.close();
+            }
+        });
+        
+        dialogLayout.setActions(ok,cancel);
+        dialog.show();
+        
+                
+    }
 }
